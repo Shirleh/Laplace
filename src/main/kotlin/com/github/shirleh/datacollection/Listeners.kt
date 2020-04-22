c2c00440ea19ebfe3ad05fec8bdbae5b9bb68b48
shirleh <shirlehxo@protonmail.com>
2020-04-22 22:06:15 +0200

    Implement data collection from new messages

--- src/main/kotlin/com/github/shirleh/datacollection/Listeners.kt
+++ src/main/kotlin/com/github/shirleh/datacollection/Listeners.kt
@@ -5,13 +5,11 @@ import com.influxdb.client.write.Point
 import discord4j.core.DiscordClient
 import discord4j.core.event.domain.guild.MemberJoinEvent
 import discord4j.core.event.domain.message.MessageCreateEvent
-import kotlinx.coroutines.coroutineScope
+import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.flow.launchIn
 import kotlinx.coroutines.flow.onEach
 import kotlinx.coroutines.reactive.asFlow
 import mu.KotlinLogging
-import java.time.Instant
-import kotlin.random.Random
 
 private val logger = KotlinLogging.logger { }
 
@@ -19,14 +17,22 @@ suspend fun addDataCollectionListeners(client: DiscordClient) = coroutineScope {
     val pointRepository = PointRepositoryImpl()
 
     client.eventDispatcher.on(MessageCreateEvent::class.java).asFlow()
-        .onEach {
-            logger.trace { "Collecting MessageCreateEvent... " }
+        .onEach { event ->
+            logger.trace { "Collecting data from MessageCreateEvent..." }
 
-            Point.measurement("cupcakes")
-                .addTag("color", "pink")
-                .addField("score", Random.nextInt())
-                .time(Instant.now(), WritePrecision.S)
+            val channelId = event.message.channelId.asString()
+            val authorId = event.message.author.map { it.id.asString() }.orElse(null) ?: return@onEach
+            val content = event.message.content.orElse(null) ?: return@onEach
+            val timestamp = event.message.timestamp
+
+            Point.measurement("message")
+                .addTag("channel", channelId)
+                .addTag("author", authorId)
+                .addField("length", content.length)
+                .time(timestamp, WritePrecision.S)
                 .let { pointRepository.save(it) }
+
+            logger.trace { "Collected data from MessageCreateEvent" }
         }
         .launchIn(this)
 
