56b708cd3ac93f3589a76d434629a68e9fbf77f6
Shirley <shirlehxo@protonmail.com>
2020-06-23 17:06:12 +0200

    docs: document `AUDIT_LOG_UPDATE_DELAY`

--- src/main/kotlin/com/github/shirleh/datacollection/DataCollectionHandler.kt
+++ src/main/kotlin/com/github/shirleh/datacollection/DataCollectionHandler.kt
@@ -18,6 +18,12 @@ import mu.KotlinLogging
 import java.time.Duration
 import java.time.Instant
 
+/**
+ * Arbitrary delay to prevent audit log access before it's updated.
+ *
+ * In Discord, there's a delay between dispatching an event and updating the audit log with said event.
+ * Larger guilds tend to have longer delays. 5 seconds is commonly used which works *most of the time*.
+ */
 private const val AUDIT_LOG_UPDATE_DELAY = 5000L
 
 object DataCollectionHandler {
