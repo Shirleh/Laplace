94857043db8c7ead1fcc71bc02c5885e313f267c
Shirley <shirlehxo@protonmail.com>
2020-10-09 17:31:09 +0200

    refactor: extract `NicknameDataCollector` from `DataCollectionHandler`

--- src/main/kotlin/com/github/shirleh/datacollection/DataCollectionHandler.kt
+++ src/main/kotlin/com/github/shirleh/datacollection/DataCollectionHandler.kt
@@ -1,24 +1,11 @@
 package com.github.shirleh.datacollection
 
-import com.github.shirleh.extensions.orElseNull
 import com.influxdb.client.domain.WritePrecision
 import com.influxdb.client.write.Point
-import discord4j.common.util.Snowflake
-import discord4j.core.`object`.audit.ActionType
-import discord4j.core.`object`.audit.AuditLogEntry
-import discord4j.core.`object`.audit.ChangeKey
 import discord4j.core.event.domain.VoiceStateUpdateEvent
-import discord4j.core.event.domain.guild.BanEvent
-import discord4j.core.event.domain.guild.MemberJoinEvent
-import discord4j.core.event.domain.guild.MemberLeaveEvent
-import discord4j.core.event.domain.guild.MemberUpdateEvent
-import discord4j.core.event.domain.message.MessageCreateEvent
-import kotlinx.coroutines.delay
-import kotlinx.coroutines.flow.*
-import kotlinx.coroutines.reactive.asFlow
-import kotlinx.coroutines.reactive.awaitSingle
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.collect
 import mu.KotlinLogging
-import java.time.Duration
 import java.time.Instant
 
 object DataCollectionHandler {
@@ -26,50 +13,6 @@ object DataCollectionHandler {
     private val logger = KotlinLogging.logger { }
 
     private val dataPointRepository = DataPointRepositoryImpl()
-
-    /**
-     * Collects member nickname data from the incoming [events].
-     */
-    suspend fun collectNicknameData(events: Flow<MemberUpdateEvent>) =
-        events
-            .onEach { delay(AUDIT_LOG_UPDATE_DELAY) }
-            .mapNotNull { findAuditLogByEvent(it) }
-            .collect { saveNicknameData(it) }
-
-    private suspend fun findAuditLogByEvent(event: MemberUpdateEvent): AuditLogEntry? {
-        logger.entry(event)
-
-        val result = event.guild.awaitSingle()
-            .getAuditLog { spec -> spec.setActionType(ActionType.MEMBER_UPDATE) }
-            .asFlow()
-            .filter { auditLogEntry ->
-                auditLogEntry.getChange(ChangeKey.USER_NICK)
-                    .map { it.currentValue == event.currentNickname }
-                    .orElse(false)
-            }
-            .firstOrNull()
-
-        return result.also { logger.exit(it) }
-    }
-
-    private fun saveNicknameData(auditLog: AuditLogEntry) {
-        logger.entry(auditLog)
-
-        val author = auditLog.responsibleUserId.asString()
-
-        val change = auditLog.getChange(ChangeKey.USER_NICK).orElseNull() ?: return
-        val oldNick = change.oldValue.orElse("")
-        val currentNick = change.currentValue.orElse("")
-
-        Point.measurement("nicknames")
-            .addTag("author", author)
-            .addField("oldNick", oldNick)
-            .addField("currentNick", currentNick)
-            .time(Instant.now(), WritePrecision.S)
-            .let { dataPointRepository.save(it) }
-
-        logger.exit()
-    }
 
     /**
      * Collects voice data from the incoming [events].
---
+++ src/main/kotlin/com/github/shirleh/datacollection/NicknameDataCollector.kt
@@ -0,0 +1,71 @@
+package com.github.shirleh.datacollection
+
+import com.github.shirleh.extensions.orElseNull
+import com.influxdb.client.domain.WritePrecision
+import com.influxdb.client.write.Point
+import discord4j.core.`object`.audit.ActionType
+import discord4j.core.`object`.audit.ChangeKey
+import discord4j.core.event.domain.guild.MemberUpdateEvent
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.*
+import kotlinx.coroutines.reactive.asFlow
+import kotlinx.coroutines.reactive.awaitSingle
+import mu.KotlinLogging
+import org.koin.core.KoinComponent
+import org.koin.core.inject
+import java.time.Instant
+
+private data class NicknameChange(val author: String, val oldNickname: String, val newNickname: String)
+
+object NicknameDataCollector : KoinComponent {
+
+    private val logger = KotlinLogging.logger { }
+
+    private val dataPointRepository: DataPointRepository by inject()
+
+    /**
+     * Collects member nickname data from the incoming [events].
+     */
+    suspend fun collect(events: Flow<MemberUpdateEvent>) =
+        events
+            .onEach { delay(AUDIT_LOG_UPDATE_DELAY) }
+            .mapNotNull(::findNicknameChange)
+            .map(::toDataPoint)
+            .collect(dataPointRepository::save)
+
+    private suspend fun findNicknameChange(event: MemberUpdateEvent): NicknameChange? {
+        logger.entry(event)
+
+        val result = event.guild.awaitSingle()
+            .getAuditLog { spec -> spec.setActionType(ActionType.MEMBER_UPDATE) }
+            .asFlow()
+            .mapNotNull { auditLogEntry ->
+                auditLogEntry
+                    .getChange(ChangeKey.USER_NICK)
+                    .map {
+                        NicknameChange(
+                            author = auditLogEntry.responsibleUserId.asString(),
+                            oldNickname = it.oldValue.orElse(""),
+                            newNickname = it.currentValue.orElse("")
+                        )
+                    }
+                    .orElseNull()
+            }
+            .filter { change -> event.currentNickname.map { it == change.newNickname }.orElse(false) }
+            .firstOrNull()
+
+        return logger.exit(result)
+    }
+
+    private fun toDataPoint(change: NicknameChange): Point {
+        logger.entry(change)
+
+        val result = Point.measurement("nickname")
+            .addTag("author", change.author)
+            .addField("oldNickname", change.oldNickname)
+            .addField("currentNickname", change.newNickname)
+            .time(Instant.now(), WritePrecision.S)
+
+        return logger.exit(result)
+    }
+}
