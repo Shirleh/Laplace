966b7d0696dfe82c9f1a95161b70e04186622897
Shirley <shirlehxo@protonmail.com>
2020-10-09 11:55:51 +0200

    refactor: add common Mono extension functions

--- src/main/kotlin/com/github/shirleh/HealthCheckCommands.kt
+++ src/main/kotlin/com/github/shirleh/HealthCheckCommands.kt
@@ -2,8 +2,8 @@ package com.github.shirleh
 
 import com.github.shirleh.command.cli.AbstractCommand
 import com.github.shirleh.command.cli.AbstractCommandCategory
+import com.github.shirleh.extensions.await
 import discord4j.core.event.domain.message.MessageCreateEvent
-import kotlinx.coroutines.reactive.awaitFirst
 import java.lang.management.ManagementFactory
 import java.time.Duration
 
@@ -11,8 +11,8 @@ class HealthCheck : AbstractCommandCategory(name = "health")
 
 class PingCommand : AbstractCommand() {
     override suspend fun execute(event: MessageCreateEvent) {
-        val channel = event.message.channel.awaitFirst()
-        channel.createMessage("Pong!").awaitFirst()
+        val channel = event.message.channel.await()
+        channel.createMessage("Pong!").await()
     }
 }
 
@@ -25,7 +25,7 @@ class UptimeCommand : AbstractCommand() {
         val minutes = duration.toMinutesPart()
         val seconds = duration.toSecondsPart()
 
-        val channel = event.message.channel.awaitFirst()
-        channel.createMessage("""$days day(s), $hours hour(s), $minutes minute(s), $seconds second(s)""").awaitFirst()
+        val channel = event.message.channel.await()
+        channel.createMessage("""$days day(s), $hours hour(s), $minutes minute(s), $seconds second(s)""").await()
     }
 }
