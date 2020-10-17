8f3a7a8631992d09ad5933644e2561e707af36fc
Shirley <shirlehxo@protonmail.com>
2020-10-18 00:00:45 +0200

    improvement: change message data metric to message count
    This is done to be more privacy-friendly. Anything more "personal" should be put behind an opt-in feature.

--- src/main/kotlin/com/github/shirleh/Main.kt
+++ src/main/kotlin/com/github/shirleh/Main.kt
@@ -6,6 +6,7 @@ import com.github.shirleh.administration.administrationModule
 import com.github.shirleh.command.CommandHandler
 import com.github.shirleh.datacollection.*
 import com.github.shirleh.datacollection.emoji.EmojiDataCollector
+import com.github.shirleh.datacollection.MessageDataCollector
 import com.github.shirleh.persistence.persistenceModule
 import discord4j.core.DiscordClient
 import discord4j.core.event.domain.Event
