dc81fcdf1cdf2e1fd4bb21a07e58bddc6e82bc3f
Shirley <48168387+Shirleh@users.noreply.github.com>
2020-10-17 14:23:54 +0200

    bug: load config externally through docker-compose instead (#51)
    * improvement: change application.conf to be environment agnostic
    
    * improvement: add development and production docker-compose files

--- src/main/kotlin/com/github/shirleh/Configuration.kt
+++ src/main/kotlin/com/github/shirleh/Configuration.kt
@@ -3,7 +3,7 @@ package com.github.shirleh
 import com.typesafe.config.Config
 
 class Configuration(config: Config) {
-    val superuserRoleId: Long = config.getLong("superuserRoleId")
+    val superuserRoleId: Long = config.getLong("discord.superuserRoleId")
 
     val influxUrl: String = config.getString("influx2.url")
     val influxDefaultOrg: String = config.getString("influx2.org")
--- src/main/kotlin/com/github/shirleh/Module.kt
+++ src/main/kotlin/com/github/shirleh/Module.kt
@@ -4,10 +4,6 @@ import com.typesafe.config.ConfigFactory
 import org.koin.dsl.module
 
 val mainModule = module {
-    single(createdAtStart = true) {
-        val dev = ConfigFactory.load()
-        val prod = ConfigFactory.load("application.prod")
-        val result = prod.withFallback(dev)
-        Configuration(result)
-    }
+    single(createdAtStart = true) { ConfigFactory.load() }
+    single(createdAtStart = true) { Configuration(get()) }
 }
