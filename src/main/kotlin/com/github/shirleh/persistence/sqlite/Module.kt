da9895bc6b72757967a37f52e9d6f5cec0785f06
Shirley <shirlehxo@protonmail.com>
2020-10-18 23:36:40 +0200

    feat: enable SQLite url to be configurable

---
+++ src/main/kotlin/com/github/shirleh/persistence/sqlite/Module.kt
@@ -0,0 +1,19 @@
+package com.github.shirleh.persistence.sqlite
+
+import com.github.shirleh.administration.Channels
+import com.github.shirleh.administration.Guilds
+import org.jetbrains.exposed.sql.Database
+import org.jetbrains.exposed.sql.SchemaUtils
+import org.jetbrains.exposed.sql.transactions.transaction
+import org.koin.dsl.module
+
+val sqliteModule = module {
+    single(createdAtStart = true) { SqliteConfiguration(get()) }
+    single(createdAtStart = true) { initSqlite(get()) }
+}
+
+private fun initSqlite(config: SqliteConfiguration): Database {
+    val result = Database.connect(config.url, "org.sqlite.JDBC")
+    transaction { SchemaUtils.createMissingTablesAndColumns(Guilds, Channels) }
+    return result
+}
