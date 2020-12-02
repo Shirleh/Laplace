a582c7b6b23cd682f7c62a027c0c97fed7b0e6f6
Shirley <shirlehxo@protonmail.com>
2020-12-02 23:37:57 +0100

    improv: add personal and channel options to message activity command

--- src/main/kotlin/com/github/shirleh/statistics/message/MessageStatisticCommands.kt
+++ src/main/kotlin/com/github/shirleh/statistics/message/MessageStatisticCommands.kt
@@ -1,7 +1,9 @@
 package com.github.shirleh.statistics.message
 
 import com.github.ajalt.clikt.parameters.options.default
+import com.github.ajalt.clikt.parameters.options.flag
 import com.github.ajalt.clikt.parameters.options.option
+import com.github.ajalt.clikt.parameters.options.validate
 import com.github.ajalt.clikt.parameters.types.long
 import com.github.shirleh.command.cli.AbstractCommand
 import com.github.shirleh.command.cli.AbstractCommandCategory
@@ -9,8 +11,9 @@ import com.github.shirleh.extensions.await
 import com.github.shirleh.extensions.orElseNull
 import com.influxdb.client.kotlin.QueryKotlinApi
 import com.influxdb.query.dsl.Flux
-import com.influxdb.query.dsl.functions.restriction.Restrictions
+import com.influxdb.query.dsl.functions.restriction.Restrictions.*
 import discord4j.common.util.Snowflake
+import discord4j.core.`object`.entity.User
 import discord4j.core.event.domain.message.MessageCreateEvent
 import discord4j.rest.util.Color
 import kotlinx.coroutines.ExperimentalCoroutinesApi
@@ -37,32 +40,40 @@ class ActivityCommand : AbstractCommand(
             require(it > 0) { "time range must be 1 or higher" }
             require(it <= 30) { "time range must be 30 or lower" }
         }
+    private val isPersonal by option("--me", help = "show only your personal statistics").flag()
+    private val channelFilter by option("-c", "--channel", help = "filter statistics by the given channel")
 
     @OptIn(ExperimentalCoroutinesApi::class)
     override suspend fun execute(event: MessageCreateEvent) {
+        logger.entry(event)
+
         val guildId = event.guildId.map(Snowflake::asString).orElseNull() ?: return
+        val userId = event.message.author.map(User::getId).orElseNull() ?: return
+        val channelId = channelFilter?.removeSurrounding("<#", ">")
 
         val flux = Flux.from("raw_discord_data")
             .range(-range, ChronoUnit.DAYS)
             .filter(measurement().equal("message"))
             .filter(field().equal("wordCount"))
             .filter(tag("guildId").equal(guildId))
+            .let { if (isPersonal) it.filter(tag("userId").equal(userId.asString())) else it }
+            .let { if (channelId != null) it.filter(tag("channel").equal(channelId)) else it }
             .groupBy("channel")
             .sum()
             .sort(true)
             .limit(10)
         val topChannels = flux.toString().also { logger.debug { "Query: $it" } }
-        val fluxRecords = queryApi.query(topChannels)
 
-        val channel = event.message.channel.await()
+        val fluxRecords = queryApi.query(topChannels)
         val results = mutableMapOf<String, Long>().apply {
             fluxRecords.consumeEach {
-                val channelId = "<#${it.getValueByKey("channel")}>"
+                val channel = "<#${it.getValueByKey("channel")}>"
                 val count = it.value as Long
-                put(channelId, count)
+                put(channel, count)
             }
         }
-        channel
+
+        event.message.channel.await()
             .createEmbed { spec ->
                 val channelList = results.keys
                     .fold(StringBuilder()) { acc, s -> acc.appendLine(s) }
@@ -76,5 +87,7 @@ class ActivityCommand : AbstractCommand(
                     .setColor(Color.WHITE)
             }
             .await()
+
+        logger.exit()
     }
 }
